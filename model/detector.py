844f83c0c2f30d194ae3f96abf7a919bf23ddfe2
Lee Christmas <monishukla727538@gmail.com>
2025-12-02 21:49:39 +0530

    Update detector.py

--- model/detector.py
+++ model/detector.py
@@ -1,1 +1,9 @@
+def detect_ip(ip: str) -> bool:
+    if ip.startswith(("10.", "192.168.", "203.0.113.")):
+        return True
+    if "::" in ip:
+        return True
+    if len(ip) > 30:
+        return True
+    return False
 
